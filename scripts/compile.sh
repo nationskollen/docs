67b3e0d6090b21df4a2072007064f3852687d353
Fredrik Engstrand <frewacom@gmail.com>
2021-04-20 06:38:33 +0200

    Added API documentation

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,24 +5,31 @@ CURRENT_PATH=$(pwd)
 mkdir -p "$CURRENT_PATH/public"
 
 # $1 - path to project
-# $2 - folder name to store the documentation in
+# $2 - outputted folder name
+# $3 - folder name to store the documentation in
+# $4 - npm task for generating documentation
 function generate_and_copy() {
     if [[ ! -d "$1" ]]; then
-        echo "ERROR: Could not find directory for $2."
-        echo "ERROR: Expected $2 to be located at $1"
+        echo "ERROR: Could not find directory for $3."
+        echo "ERROR: Expected $3 to be located at $1 in $2"
     fi
 
     cd "$1"
 
-    npm run docs
+    npm run $4
 
-    rm -rf "$CURRENT_PATH/public/$2"
-    mv docs/ "$CURRENT_PATH/public/$2"
+    rm -rf "$CURRENT_PATH/public/$3"
+    mv "$2" "$CURRENT_PATH/public/$3"
 
     cd "$CURRENT_PATH"
 }
 
-generate_and_copy "../sdk" "sdk"
-generate_and_copy "../server" "server"
+generate_and_copy "../sdk" "docs" "sdk" "docs"
+generate_and_copy "../server" "docs" "server" "docs"
+generate_and_copy "../server" "docs-api" "api" "docs:api"
+
+# Insomnia documenter expects the config to be located at
+# the root of the webserver.
+mv "$CURRENT_PATH/public/api/insomnia.json" "$CURRENT_PATH/public/insomnia.json"
 
 cp "$CURRENT_PATH/static/index.html" "$CURRENT_PATH/public/index.html"
